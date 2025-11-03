55a07d3854573e6d9132f8947e8e9828a7cb431c
gcasinzapata-design <gc.asin.zapata@gmail.com>
2025-11-03 16:49:48 -0500

    Fix merge conflict in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } }
-=======
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } }
->>>>>>> 498bff203ccdfeec93abf50005e4921202812e2d
