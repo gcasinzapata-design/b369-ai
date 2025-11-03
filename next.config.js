de934e1717ec47e42321b5f9188dafa346c8a912
gcasinzapata-design <gc.asin.zapata@gmail.com>
2025-11-03 16:34:43 -0500

    Simplify next.config.js by removing experimental options
    Removed experimental appDir configuration and comments.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
-  // No cambies nada aquí hasta que el deploy pase
-  // Si luego agregamos mapas/scraping, ajustamos aquí con calma
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
